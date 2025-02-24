61aa2f34bc72e97eab313f10c3fe0b6a2b4fb984
Raphael Royaute <raphael.royaute@inrae.fr>
2025-02-24 16:25:43 +0100

    Change simulation parameters & add plot script

--- R/sims/sims_stan.R
+++ R/sims/sims_stan.R
@@ -2,6 +2,7 @@
 library(tidyverse); library(here); library(cmdstanr); 
 library(bayesplot); library(plotly); library(mvtnorm); 
 library(shinystan); library(tidybayes); library(posterior)
+library(easystats); library(patchwork)
 
 source("R/funs/generate_mvn.R")
 
@@ -74,12 +75,12 @@ source("R/funs/generate_mvn.R")
 # Data simulation parameter
 set.seed(42)
 
-n_obs = 60 # number of observation per discret point in the gradient
-x = rep(c(-1,-0.8,-0.6,-0.4,-0.2, 0, 0.2,0.4,0.6,0.8, 1), each = n_obs) # environmental gradent
+n_obs = 30 # number of observation per discret point in the gradient
+x = rep(c(-1,-0.8,-0.6,-0.4,-0.2, 0, 0.2,0.4,0.6,0.8, 1), each = n_obs) # environmental gradient
 
 # Mean parameters
 beta0 = c(1, -1) # vector of intercepts
-beta1 = c(0.6, 0.4) # vector of slopes 
+beta1 = c(0.6, -0.4) # vector of slopes 
 
 # Dispersion parameters
 beta0_disp = c(0, 0, 0) # vector of intercepts
@@ -102,7 +103,7 @@ df_sim = df_obs %>%
 
 df_sim = cbind(df_obs, df_sim)
 
-#write.csv(df_sim, "data/data-sims/df_sim.csv", row.names = F)
+write.csv(df_sim, "data/data-sims/df_sim.csv", row.names = F)
 
 plot(Act ~ Exp, df_sim)
 plot(Act ~ x, df_sim)
@@ -211,118 +212,180 @@ fit_crn$summary(variables = c("B_m_a", "B_m_e", "B_cpc", "sd_G"))
 # print(fit_crn)
 # 
 ## Inspect the model -----
-
-#### The following 2 lines of code work fine for me, it doesn't throw any error.
-# maybe add the "output_dir" command in the code above in case the issue was 
-# because it saved the model output in a temporary folder
-
-fit <- read_cmdstan_csv(fit_crn$output_files(),
-                        variables = c("B_m_a", "B_m_e", "B_cpc", "sd_G")) 
-post_draws <- posterior::as_draws_df(fit$post_warmup_draws)  
+fit_crn <- readRDS("outputs/mods/sims/fit_crn.RDS")
 
 post_draws <- as_draws_df(fit_crn$draws())
 post_draws <- subset_draws(post_draws,
                            variable =
                              c("B_mq_a", "B_mq_e", "B_cpcq",
                                "sd_G", "B_m_a", "B_m_e", "B_cpc"))
+get_variables(post_draws)
+summarise_draws(post_draws)
+mcmc_trace(post_draws)
+mcmc_rank_overlay(post_draws)
+mcmc_rank_ecdf(post_draws)
 
 
-# post_draws contain the posterior distributions for all your parameters of 
-# interest (intercept and slope for each of the 2 traits and for the correlation
-# between both traits). This is not the right format to directly use Jordan's 
-# post processing functions, but in the end i don't think his functions are needed,
-# what they do is reshape the output to a specific format to make plots.
-# I think it is easier to use the output format that you already have in 
-# "postdraws", because with these you already have everything you need to make
-# predictions and plots.
-
-
-#### Plotting effect of your environmental gradient on the correlation between both traits
-
+### Plots ----
 x2.sim = seq(min(stan.df$X[,2]),
              max(stan.df$X[,2]),
              by =  0.1) 
+int.sim <- matrix(rep(NA, nrow(post_draws)*length(x2.sim)), 
+                  nrow = nrow(post_draws))
 
+### Coefficient plot (TODO) ----
+
+### Plotting the change in mean with the gradient ----
+
+#### Activity ----
 # predict the correlation across the range of your environmental value
-int.sim <- matrix(rep(NA, nrow(post_draws)*length(x2.sim)), nrow = nrow(post_draws))
+for(i in 1:length(x2.sim)){
+  int.sim[, i] <- post_draws$`B_m_a[1]` + post_draws$`B_m_a[2]` * (x2.sim[i])
+}
+
+
+# calculate quantiles of predictions
+df.post.summary = int.sim %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:21) %>% 
+  mutate(Dose = rep(x2.sim, nrow(post_draws))) %>% 
+  group_by(Dose) %>% 
+  median_qi(.width = c(.5, .8, .95))
+df.post.summary
+
+fig_act_mu = int.sim %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:21) %>% 
+  mutate(Dose = rep(x2.sim, nrow(post_draws))) %>% 
+  ggplot(aes(x = Dose, y = value)) +
+  stat_lineribbon() +
+  scale_fill_brewer() +
+  # ylim(-1,1)+
+  xlab("Environmetal gradient")+
+  ylab("Average Activity")+
+  theme_bw() +
+  theme(legend.position = "none")
+fig_act_mu
+
+#### Exploration ----
+# predict the correlation across the range of your environmental value
+for(i in 1:length(x2.sim)){
+  int.sim[, i] <- post_draws$`B_m_e[1]` + post_draws$`B_m_e[2]` * (x2.sim[i])
+}
+
+
+# calculate quantiles of predictions
+df.post.summary = int.sim %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:21) %>% 
+  mutate(Dose = rep(x2.sim, nrow(post_draws))) %>% 
+  group_by(Dose) %>% 
+  median_qi(.width = c(.5, .8, .95))
+df.post.summary
+
+fig_explo_mu = int.sim %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:21) %>% 
+  mutate(Dose = rep(x2.sim, nrow(post_draws))) %>% 
+  ggplot(aes(x = Dose, y = value)) +
+  stat_lineribbon() +
+  scale_fill_brewer() +
+  # ylim(-1,1)+
+  xlab("Environmetal gradient")+
+  ylab("Average Exploration")+
+  theme_bw() +
+  theme(legend.position = "none")
+fig_explo_mu
+
+### Plotting the change in variance with the gradient ----
+#### Activity ----
+# calculate quantiles of predictions
+df.post.summary = post_draws[,7:17] %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:11) %>% 
+  mutate(Dose = rep(c(-1,-0.8,-0.6,-0.4,-0.2, 0, 0.2,0.4,0.6,0.8, 1), 
+                    nrow(post_draws))) %>% 
+  group_by(Dose) %>% 
+  median_qi(.width = c(.5, .8, .95))
+
+
+fig_act_sd = post_draws[,7:17] %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:11) %>% 
+  mutate(Dose = rep(c(-1,-0.8,-0.6,-0.4,-0.2, 0, 0.2,0.4,0.6,0.8, 1), 
+                    nrow(post_draws))) %>%  
+  ggplot(aes(x = Dose, y = value)) +
+  stat_halfeye() +
+  scale_fill_brewer() +
+  xlab("Environmetal gradient")+
+  ylab("Activity sd")+
+  theme_bw() +
+  theme(legend.position = "none")
+fig_act_sd
+
+#### Exploration ----
+# calculate quantiles of predictions
+df.post.summary = post_draws[,18:28] %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:11) %>% 
+  mutate(Dose = rep(c(-1,-0.8,-0.6,-0.4,-0.2, 0, 0.2,0.4,0.6,0.8, 1), 
+                    nrow(post_draws))) %>% 
+  group_by(Dose) %>% 
+  median_qi(.width = c(.5, .8, .95))
+
+
+fig_explo_sd = post_draws[,18:28] %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:11) %>% 
+  mutate(Dose = rep(c(-1,-0.8,-0.6,-0.4,-0.2, 0, 0.2,0.4,0.6,0.8, 1), 
+                    nrow(post_draws))) %>%  
+  ggplot(aes(x = Dose, y = value)) +
+  stat_halfeye() +
+  scale_fill_brewer() +
+  xlab("Environmetal gradient")+
+  ylab("Exploration sd")+
+  theme_bw() +
+  theme(legend.position = "none")
+fig_explo_sd
+
+### Plotting the change in correlation with the gradient ----
 for(i in 1:length(x2.sim)){
   int.sim[, i] <- tanh(post_draws$`B_cpc[1,1]` + post_draws$`B_cpc[2,1]` * (x2.sim[i])) 
 }
 
 # calculate quantiles of predictions
-bayes.c.eff.median <- tanh(median(post_draws$`B_cpc[1,1]`) + median(post_draws$`B_cpc[2,1]`) * (x2.sim)) 
-bayes.c.eff.lower <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.05)))
-bayes.c.eff.upper <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.95)))
-bayes.c.eff.lower.bis <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.15)))
-bayes.c.eff.upper.bis <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.85)))
-bayes.c.eff.lower.bis2 <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.25)))
-bayes.c.eff.upper.bis2 <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.75)))
-bayes.c.eff.lower.bis3 <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.35)))
-bayes.c.eff.upper.bis3 <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.65)))
-bayes.c.eff.lower.bis4 <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.45)))
-bayes.c.eff.upper.bis4 <- apply(int.sim, 2, function(x) quantile(x, probs = c(0.55)))
-plot.dat <- data.frame(x2.sim, bayes.c.eff.median,
-                       bayes.c.eff.lower, bayes.c.eff.upper,
-                       bayes.c.eff.lower.bis, bayes.c.eff.upper.bis,
-                       bayes.c.eff.lower.bis2, bayes.c.eff.upper.bis2,
-                       bayes.c.eff.lower.bis3, bayes.c.eff.upper.bis3,
-                       bayes.c.eff.lower.bis4, bayes.c.eff.upper.bis4)
+df.post.summary = int.sim %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:21) %>% 
+  mutate(Dose = rep(x2.sim, nrow(post_draws))) %>% 
+  group_by(Dose) %>% 
+  median_qi(.width = c(.5, .8, .95))
 
-
-p <- ggplot(plot.dat, aes(x = x2.sim, y = bayes.c.eff.median)) +
-  geom_ribbon(aes(ymin = bayes.c.eff.lower.bis4, ymax = bayes.c.eff.upper.bis4), fill = "darkseagreen4", alpha = 0.1)+
-  geom_ribbon(aes(ymin = bayes.c.eff.lower.bis3, ymax = bayes.c.eff.upper.bis3), fill = "darkseagreen4", alpha = 0.1)+
-  geom_ribbon(aes(ymin = bayes.c.eff.lower.bis2, ymax = bayes.c.eff.upper.bis2), fill = "darkseagreen4", alpha = 0.1)+
-  geom_ribbon(aes(ymin = bayes.c.eff.lower.bis, ymax = bayes.c.eff.upper.bis), fill = "darkseagreen4", alpha = 0.1)+
-  geom_ribbon(aes(ymin = bayes.c.eff.lower, ymax = bayes.c.eff.upper), fill = "darkseagreen4", alpha = 0.1)+
-  geom_line(color = "darkseagreen4", size = 1.8, alpha=0.6)+
+fig_act_explo_corr =  int.sim %>% 
+  data.frame() %>% 
+  pivot_longer(names_to = "Dose", cols = 1:21) %>% 
+  mutate(Dose = rep(x2.sim, nrow(post_draws))) %>% 
+  ggplot(aes(x = Dose, y = value)) +
+  stat_lineribbon() +
+  scale_fill_brewer() +
   ylim(-1,1)+
   xlab("Environmetal gradient")+
   ylab("Observation-level correlation")+
   theme_bw() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())+
-  theme(plot.title = element_text(size=10, hjust = 0.5),
-        axis.text.y = element_text(size=14),
-        axis.text.x = element_text(size=14))
-p
+  theme(legend.position = "none")
+fig_act_explo_corr
 
 
+### Export figures ----
+fig_mu = fig_act_mu + fig_explo_mu
+fig_sd = fig_act_sd + fig_explo_sd
 
+fig_all_effects = 
+  (fig_act_mu + fig_explo_mu) /
+  (fig_act_sd + fig_explo_sd) /
+  fig_act_explo_corr
 
-# fit_crn$metadata()$model_params
-# # # launch_shinystan(fit_crn)
-# # csv_paths = fit_crn$output_files()
-# # CRN_fit = rstan::read_stan_csv(fit_crn$output_files())
-# # saveRDS(CRN_fit, "CRN_fit.RDS")
-# # NOT WORKING!
-# # Trying to find an equivalent postprocessing procedure using {posterior} or {tidybayes}
-# # What would replace the extract(CRN_fit) line so that I could follow the rest of Jordan's tutorial?
-# 
-# 
-# ## Plots ----
-# mcmc_hist(fit_crn$draws("B_m_a[1]"))
-# draws <- posterior::as_draws_rvars(fit_crn$draws())
-# draws <- posterior::as_draws_array(fit_crn$draws())
-# mcmc_hist(draws$B_m_a)
-# summarise_draws(tidy_draws(fit_crn$draws()))
-# get_variables(fit_crn$draws())
+ggsave("outputs/figs/fig_mu.png", fig_mu)
+ggsave("outputs/figs/fig_sd.png", fig_sd)
+ggsave("outputs/figs/fig_all_effects.png", fig_all_effects)
 
-## Plots ----
-# summarise_draws(tidybayes::tidy_draws(fit_crn$draws()))
-# get_variables(fit_crn$draws())
-# mcmc_hist(fit_crn$draws("B_m_a[1]"))
-# 
-# draws <- as_draws_rvars(fit_crn$draws())
-# draws <- as_draws_array(fit_crn$draws())
-# draws <- as_draws_df(fit_crn$draws())
-# df_draws <- subset_draws(draws, 
-#                          variable = 
-#                            c("B_mq_a", "B_mq_e", "B_cpcq", 
-#                              "sd_G", "B_m_a", "B_m_e", "B_cpc"))
-# 
-# 
-# mcmc_trace(df_draws)
-# # mcmc_rank_hist(df_draws)
-# mcmc_rank_overlay(df_draws)
-# mcmc_rank_ecdf(df_draws)
-# summarise_draws(df_draws)
