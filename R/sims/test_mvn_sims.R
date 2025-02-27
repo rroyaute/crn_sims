926481d57ea0ea41953bdace59284301abd99e86
Raphael Royaute <raphael.royaute@inrae.fr>
2025-02-27 15:45:53 +0100

    test on dose = 0 and dose = 30

--- R/sims/test_mvn_sims.R
+++ R/sims/test_mvn_sims.R
@@ -1,9 +1,63 @@
-cov2cor(matrix(c(1^2, 0.07, 0.07, 0.1^2), nrow = 2))
-        
+df_obs[1,] # Dose = 0
+df_obs[120,] # Dose max  = 30
 
+cov2cor(matrix(c(1^2, 0.07, 0.07, 0.1^2), nrow = 2))
+cov2cor(matrix(c(1.020201^2, 0.05390475, 0.05390475, 0.08187308^2), 
+               nrow = 2))
+# Simulate dose = 0 ----
+## From rmvnorm() ----
 dat = 
   rmvnorm(30, c(5.010635, 0),
         matrix(c(1^2, 0.07, 0.07, 0.1^2), nrow = 2))
+
+dat = dat %>% 
+  data.frame() %>% 
+  mutate(Act = exp(X1),
+         Exp = inv_logit(X2))
+dat %>%
+  ggplot(aes(X1, X2)) +
+  geom_point()
+
+dat %>%
+  ggplot(aes(log(Act), Exp)) +
+  geom_point()
+
+dat %>%
+  ggplot(aes(Act, Exp)) +
+  geom_point()
+
+cor(dat$X1, dat$X2)
+cor(log(dat$Act), dat$Exp)
+
+## From generate_mvn() ----
+dat = df_obs[1:30,] %>% 
+  pmap_dfr(~generate_mvn(c(...)), .id = "row_id") 
+
+dat = dat %>% 
+  data.frame() %>% 
+  mutate(Act = exp(y1),
+         Exp = inv_logit(y2))
+dat %>%
+  ggplot(aes(y1, y2)) +
+  geom_point()
+
+dat %>%
+  ggplot(aes(log(Act), Exp)) +
+  geom_point()
+
+dat %>%
+  ggplot(aes(Act, Exp)) +
+  geom_point()
+
+cor(dat$y1, dat$y2)
+cor(log(dat$Act), dat$Exp)
+
+
+# Simulate dose = 30 ----
+## From rmvnorm() ----
+dat = 
+  rmvnorm(30, c(5.002603, -0.3227734),
+          matrix(c(1^2, 0.07, 0.07, 0.1^2), nrow = 2))
 
 
 dat = dat %>% 
@@ -26,3 +80,28 @@ dat %>%
 
 cor(dat$X1, dat$X2)
 cor(log(dat$Act), dat$Exp)
+
+## From generate_mvn() ----
+dat = df_obs[120:150,] %>% 
+  pmap_dfr(~generate_mvn(c(...)), .id = "row_id") 
+
+dat = dat %>% 
+  data.frame() %>% 
+  mutate(Act = exp(y1),
+         Exp = inv_logit(y2))
+dat %>%
+  ggplot(aes(y1, y2)) +
+  geom_point()
+
+dat %>%
+  ggplot(aes(log(Act), Exp)) +
+  geom_point()
+
+dat %>%
+  ggplot(aes(Act, Exp)) +
+  geom_point()
+
+cor(dat$y1, dat$y2)
+cor(log(dat$Act), dat$Exp)
+
+
